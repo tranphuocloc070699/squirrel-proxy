b99881313b34d8fe15bb45341edfd7e335bfdab0
tranphuocloc070699 <tranphuocloc070699@gmail.com>
2023-11-05 21:09:52 +0700

    home

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import express from "express";
 import cors from "cors";
+import jsonwebtoken from 'jsonwebtoken';
 import {
   createProxyMiddleware,
   Filter,
@@ -18,8 +19,10 @@ app.use(
     target: "http://localhost:8080",
     changeOrigin: true,
     pathRewrite: (path, req) => {
-      console.log(path + " -" + req.method + " - " + req.body);
-  
+      console.log(req.headers)
+      const token : string = req.headers['token'] as string
+      const response =  jsonwebtoken.verify(token,"tranphuoclocvothikim070699160400tranphuoclocvothikim070699160400")
+      console.log({response})
       return path.replace("/api", "");
     },
     onProxyReq:fixRequestBody
